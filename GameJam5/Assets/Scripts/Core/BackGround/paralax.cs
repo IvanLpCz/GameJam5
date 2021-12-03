6a61d11efa628815b00fc3d2b7abfc8c51e91ee0
IvanLpCz <ivanlopezcozar@gmail.com>
2021-12-03 01:02:07 +0100

    Shader TV

--- GameJam5/Assets/Scripts/Core/BackGround/paralax.cs
+++ GameJam5/Assets/Scripts/Core/BackGround/paralax.cs
@@ -13,12 +13,13 @@ public class paralax : MonoBehaviour
         startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    private void FixedUpdate()
+    private void Update()
     {
         float temp = (cam.transform.position.x * (1 - paralaxEffect));
         float dist = (cam.transform.position.x * paralaxEffect);
-
+        
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
         if(temp > startPos + length)
         {
             startPos += length;
